cad1d2ed5128c53b7aa5df6d9bfd587ec656379b
KKaeBu <h2sorginal@gmail.com>
2025-05-21 11:11:01 +0900

    FIX: front vue port cors, web open

--- src/main/java/com/ssafy/logoserver/config/WebConfig.java
+++ src/main/java/com/ssafy/logoserver/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:8081")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:8090")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization") // Authorization 헤더 노출 추가
