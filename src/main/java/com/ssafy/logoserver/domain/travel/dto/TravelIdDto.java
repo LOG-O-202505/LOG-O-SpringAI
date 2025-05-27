cc867c0e4722dac00ef3b36df0f154d4e5e7429c
KKaeBu <h2sorginal@gmail.com>
2025-05-28 00:23:48 +0900

    feat: Notion API 연결 완료 - Client -> Spring -> AI -> Notion 로직 구현 - 사용자 Notion page에 데이터 삽입 확인 완료

---
+++ src/main/java/com/ssafy/logoserver/domain/travel/dto/TravelIdDto.java
@@ -0,0 +1,13 @@
+package com.ssafy.logoserver.domain.travel.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+public class TravelIdDto {
+    private Long tuid;
+
+}
